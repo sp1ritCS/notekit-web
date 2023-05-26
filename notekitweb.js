78f25d719ac4c52d061bec2b80800db92eb76fdc
Florian "sp1rit"​ <sp1rit@disroot.org>
2023-05-26 09:21:46 +0200

    notekitwebjs: Implemented decompression fallback using pako

--- notekitweb.js
+++ notekitweb.js
@@ -25,7 +25,7 @@ class Stroke {
 		this.g = g;
 		this.b = b;
 		this.a = a;
-		
+
 		this.xcoords = new Float32Array(ncoords);
 		this.ycoords = new Float32Array(ncoords);
 		this.pcoords = new Float32Array(ncoords);
@@ -42,7 +42,7 @@ class NoteKitDrawing {
 	constructor(data) {
 		this.width = data[0];
 		this.height = data[1];
-		
+
 		var pos = 3;
 		this.strokes = Array(data[2]);
 		for(var nstrokes = 0; nstrokes<data[2]; nstrokes++) {
@@ -51,15 +51,15 @@ class NoteKitDrawing {
 			let b = cast_uint_float(data[pos+2]);
 			let a = cast_uint_float(data[pos+3]);
 			let ncoords = data[pos+4];
-			
+
 			const stroke = new Stroke(r, g, b, a, ncoords);
-			
+
 			for(var coord = 0; coord<ncoords; coord++) {
 				stroke.xcoords[coord] = cast_uint_float(data[pos+5+coord]);
 				stroke.ycoords[coord] = cast_uint_float(data[pos+5+coord+  ncoords]);
 				stroke.pcoords[coord] = cast_uint_float(data[pos+5+coord+2*ncoords]);
 			}
-			
+
 			this.strokes[nstrokes] = stroke;
 			pos += 5+3*ncoords;
 		}
@@ -85,15 +85,21 @@ class NoteKitDrawing {
 	}
 }
 
-function encoded_string_to_stream(encoded) {
-	// this is a terrible solution, but I guess that's to be expected from js ¯\_(ツ)_/¯
-	// Credits to Goran.it on SO; https://stackoverflow.com/a/21797381/10890264
-	// Licensed under CC-BY-SA 4.0
+// this is a terrible solution, but I guess that's to be expected from js ¯\_(ツ)_/¯
+// Credits to Goran.it on SO; https://stackoverflow.com/a/21797381/10890264
+// Licensed under CC-BY-SA 4.0
+function encoded_string_to_bytes(encoded) {
 	var binary_string = atob(encoded);
 	var bytes = new Uint8Array(binary_string.length);
 	for (var i = 0; i < binary_string.length; i++) {
 		bytes[i] = binary_string.charCodeAt(i);
 	}
+
+	return bytes;
+}
+
+function encoded_string_to_stream(encoded) {
+	const bytes = encoded_string_to_bytes(encoded);
 
 	return new ReadableStream({
 		start(controller) {
@@ -103,11 +109,47 @@ function encoded_string_to_stream(encoded) {
 	});
 }
 
-async function notekit_drawing_from_string(encoded) {
+async function notekit_drawing_from_string_native(encoded) {
 	const zlib = new DecompressionStream("deflate");
 	return new NoteKitDrawing(new Uint32Array(
 		await (
 			await new Response(encoded_string_to_stream(encoded).pipeThrough(zlib)).blob()
 		).arrayBuffer()
 	));
+}
+
+async function notekit_drawing_from_string_fallback(encoded) {
+	return new NoteKitDrawing(new Uint32Array(
+		pako.inflate(encoded_string_to_bytes(encoded)).buffer
+	));
+}
+
+var notekit_drawing_from_string = () => { return new Promise((resolve, reject) => reject("Not yet initialized.")); }
+
+function notekit_init(callback) {
+	notekit_drawing_from_string_native("eJxjYEAAAAAMAAE=")
+		.then(_drawing => {
+			console.log("Properly initialized, using native stategy.");
+			notekit_drawing_from_string = notekit_drawing_from_string_native;
+			callback("native");
+		})
+		.catch(e => {
+			console.log(`Error ${e}, falling back to fallback stategy.`);
+
+			const body = document.getElementsByTagName('body')[0];
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = "./pako_inflate.js";
+
+			const pako_loaded_callback = () => {
+				console.log("Pako was loaded, using fallback staregy.");
+				notekit_drawing_from_string = notekit_drawing_from_string_fallback;
+				callback("fallback:pako");
+			};
+
+			script.onreadystatechange = pako_loaded_callback;
+			script.onload = pako_loaded_callback;
+
+			body.appendChild(script);
+		});
 }
